00cc9f686670316016ccf955e891f56bbd7a6e0d
ohsumin <otnals25@naver.com>
2020-08-13 16:22:26 +0900

    Updates

---
+++ precache-manifest.57bb3c97715ca1bdd36fabf0d5499504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "43ff4af99402e2cf0e19",
+    "url": "/weather-app/static/js/main.126b8274.chunk.js"
+  },
+  {
+    "revision": "ab109a8cbb9b42dc3f47",
+    "url": "/weather-app/static/js/2.efe48eff.chunk.js"
+  },
+  {
+    "revision": "43ff4af99402e2cf0e19",
+    "url": "/weather-app/static/css/main.8fb63af5.chunk.css"
+  },
+  {
+    "revision": "14937c6f3feb781a6b743798c7824408",
+    "url": "/weather-app/index.html"
+  }
+];
